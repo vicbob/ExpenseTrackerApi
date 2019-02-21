b65ccf421184c3fff76b5abec1466d66ef104996
vicbob <vicbob360@gmail.com>
2019-02-21 18:25:01 +0100

    modified login response

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask
-from flask_jwt import JWT
+from flask import Flask, jsonify
 from flask_cors import CORS
+from flask_jwt import JWT
+
 import config
 from routes import routes
 from security import authenticate, identity
@@ -10,15 +11,17 @@ def setup_app(_config):
     _app = Flask(__name__)
     _app.config.from_object(_config)
     db.init_app(_app)
-    jwt = JWT(_app,authenticate,identity)
     routes(_app)
     CORS(_app)
     return _app
 
+    
+
 if __name__ == '__main__':
     from db import db
     app = setup_app(config.Config)
-
+    jwt = JWT(app, authenticate, identity)
+    
     @app.route("/")
     def hello():
         return "Hello World!"
@@ -28,7 +31,11 @@ if __name__ == '__main__':
         print("I got here")
         db.create_all()
 
+    @jwt.auth_response_handler
+    def customized_response_handler(access_token, identity):
+        return jsonify({
+            'access_token': access_token.decode('utf-8'),
+            'username': identity.username
+        })
+
     app.run(debug=True)
-
-
-
